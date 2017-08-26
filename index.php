f2069348eafbceca90ff8233f8100dcfd10c873d
ara7 <aralena7@gmail.com>
2017-08-26 16:01:49 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE HTML> 
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+   <meta charset="UTF-8">
+<title> Project </title>	
+<link type "text/css" rel = "stylesheet" href = "css/bootstrap3.css" />
+      <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
+		     <script type="text/javascript" src="js/jquery.js"></script>
+		     <script src="js/bootstrap3.js"></script>
+		     <script src="js/html5shiv.js"></script>
+			<script src="js/respond.min.js"></script>
+			<script type="text/javascript" src="js/register.js"></script>
+			<script type="image" src="img/glyphicons-halflings.png"> </script>
+			<script src="js/jquery-1.8.3.min.js" type="text/javascript"></script>
+ 
+<script type="text/javascript" src="js/placeholders.js"></script>
+ 
+
+<style>
+html, body, #map-canvas 
+{
+height: 100%;
+min-height:700px;
+}
+	 
+html, body, #button,#new_user , #forgot_password , #change_password
+{
+color: #FFFFFF;
+font-family:"Tohama",Georgia,Serif;
+}
+	  
+
+</style>
+
+
+
+</head>
+
+<body>
+ <form  class = "well form-inline"> 
+<div class="span10 form-group" >
+<label id= "ACD" style = "color: #FFFFFF;font-family:Tohama,Georgia,Serif;font-size:25px;" >Cryptography</label>
+</div>
+
+<div class = "span2 pull-right">
+
+ 
+</div>
+   
+</form>
+  <!-- Carousel
+    ================================================== -->	
+  
+                <button class="close" data-dismiss="modal" type="button">×</button>
+             
+           <center><label id="ACD" style="color: #000066;font-family:Tohama,Georgia,Serif;font-size:25px;">WELCOME</label> </center>
+              
+ 
+   
+            
+<form id="form1" class="form-horizontal" role="form" name="" action="" method="POST">                     
+   <center> <table align="">
+                   <p><a href="http://localhost/Crypto_Project/loginTx.php" class="btn btn-info" id="btnSen">Sender!</a></p>
+				   <a href="http://localhost/receiver/loginRx.php" class="btn btn-info" id="btnRec">Receiver!</a>
+                             
+	 </table></center><br>
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   
+
+   
+</form>
+    
+
+          <!--  </form>-->
+        <!-- /.modal-content -->
+     <!-- /.modal-dialog -->
+   
+      </form>
+<!-- /.modal -->
+  
+  <!-- /.modal -->
+ <!-- /.modal -->
+ 
+       </div>
+   
+       </div>
+        
+  <div class = "container" > 
+  <div class = "row" > 
+   <div class = "span8" > 
+   
+	
+    <div id="myCarousel" class="carousel slide">
+
+	<!-- /.carousel -->
+
+</div>
+</div>
+    
+
+    
+</div>
+
+
+  <hr>
+  </hr>
+
+<footer>
+ 
+</footer>
+	<script>
+		 // $(document).on("click","#btnRec",function()
+         // {
+		   // header ("Location: loginRx.php"); 
+		 // });
+	</script>
+
+</body>
+</html>
+
+
+
